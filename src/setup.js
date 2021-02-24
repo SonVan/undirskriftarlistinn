1ead068f898a005441563973d4d4bb86c3d0b5d8
SonVan <32542221+SonVan@users.noreply.github.com>
2021-02-24 21:07:52 +0000

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -5,29 +5,18 @@ const schemaFile = './sql/schema.sql';
 const fakeFile = './sql/schema.sql';
 
 async function create() {
-  const data = await readFile(schemaFile);
+  const data1 = await readFile(schemaFile);
+  const data2 = await readFile(fakeFile);
 
-  await query(data.toString('utf-8'));
+  await query(data1.toString('utf-8'));
+  await query(data2.toString('utf-8'));
 
   await end();
 
   console.info('Schema created');
-}
-
-async function insertFake() {
-  const data = await readFile(fakeFile);
-
-  await query(data.toString('utf-8'));
-
-  await end();
-
   console.info('Added fake data');
 }
 
 create().catch((err) => {
   console.error('Error creating schema', err);
-});
-
-insertFake().catch((err) => {
-  console.error('Error inserting fake data', err);
 });
